0f7773412881d8d075ee9e9a6b3705e6196700d4
tmcamillo <imeio.tmc@gmail.com>
2022-10-12 10:56:32 +0200

    first commit with setup done

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import './styles/global.css';
+
+export function App() {
+  return (
+    <>
+      <h1 className='font-bold text-2xl'>Hello World</h1>
+      <button className='bg-cyan-500 px-4 py-2 rounded text-gray-800 hover:bg-cyan-300'>Enviar</button>
+    </>
+  )
+}
