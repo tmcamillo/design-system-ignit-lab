711f3d8c4fe415205af54b7c48f5debbf0d9cecf
tmcamillo <imeio.tmc@gmail.com>
2022-10-18 14:35:53 +0200

    composition of TextInput and its documentation created

---
+++ src/components/TextInput.stories.tsx
@@ -0,0 +1,25 @@
+import { TextInput, TextInputInputProps, TextInputRootProps } from './TextInput'
+import { Meta, StoryObj } from '@storybook/react'
+import { Envelope } from 'phosphor-react'
+
+export default {
+    title: 'Components/TextInput',
+    component: TextInput.Root,
+    args: {
+        children: [
+            <TextInput.Icon> 
+                <Envelope/>
+            </TextInput.Icon>,
+            <TextInput.Input placeholder='Type your email address'/>
+        ]
+    },
+    argTypes: {
+        children: {
+            table: {
+                disable: true
+            }
+        },
+    }
+} as Meta<TextInputRootProps>
+
+export  const Default: StoryObj<TextInputRootProps> = {}
